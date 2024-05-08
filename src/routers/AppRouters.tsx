a4e111a2632ee24da77b61f22b3e5933c0bbe74e
brian Céspedes aguilera <cespedes.aguilera.brian@gmail.com>
2024-05-08 20:21:03 -0300

    add router for register page

--- src/routers/AppRouters.tsx
+++ src/routers/AppRouters.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { HomePage } from '../pages/HomePage'
+import { RegisterPage } from '../pages/RegisterPage'
 
 export const AppRouters = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path='/register' element={<RegisterPage />}/>
       </Routes>
     </BrowserRouter>
   )
