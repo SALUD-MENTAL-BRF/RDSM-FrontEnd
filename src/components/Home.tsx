c0abb5bb1944c1eb9e2a94bc5a22535aeceee0ee
brian Céspedes aguilera <cespedes.aguilera.brian@gmail.com>
2024-05-09 13:08:32 -0300

    add button in HomePage for redirect to RegisterPage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,11 @@
+
 export const Home = () => {
   return (
-    <h1>Bienvenido a la Pagina Principal</h1>
+    <>
+      <h1>Bienvenido a la Pagina Principal</h1>
+
+      <a className="btn btn-warning" href="/register">Registro</a>
+
+    </>
   )
 }
