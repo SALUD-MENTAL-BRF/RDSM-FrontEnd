c3565de066da4c834895d18332a43397e3197e23
brian Céspedes aguilera <cespedes.aguilera.brian@gmail.com>
2024-05-15 19:20:00 -0300

    add styles

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,20 @@
-import '../assets/style/Home.css'
-import { HeaderWithoutLoggedIn } from './headers/HeaderWithoutLoggedIn'
+import "../assets/style/Home.css";
+import { HeaderWithoutLoggedIn } from "./headers/HeaderWithoutLoggedIn";
 
 export const Home: React.FC = () => {
   return (
     <>
       <HeaderWithoutLoggedIn />
-      <div className="main">
-
+      <div className="Main">
+        <div className="Main__Section-text">
+          <div className="Main__Section-text-title">
+            <h1 className="abril-fatface-regular">
+              Tu bienestar mental al alcance de tu mano
+            </h1>
+          </div>
+        </div>
+        <div className="Main__Section-image"></div>
       </div>
     </>
-  )
-}
+  );
+};
