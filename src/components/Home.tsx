11d66d1219753d253602f5e2f1dd7b10bdfa8fec
brian Céspedes aguilera <cespedes.aguilera.brian@gmail.com>
2024-05-19 00:42:51 -0300

    styles

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,6 @@
 import { Header } from "./headers/Header";
-import img from '/LandingPage/img.jpg';
-import '../assets/style/Home/Home.css';
+import img from "/LandingPage/img.jpg";
+import "../assets/style/Home/Home.css";
 
 export const Home: React.FC = () => {
   return (
@@ -9,16 +9,34 @@ export const Home: React.FC = () => {
       <div className="Home__Main">
         <section className="Home__Main-hero">
           <div className="Home__Main-hero-content">
-            <h1 className="Home__Main-hero-title">Tu bienestar mental al alcance de tu mano</h1>
-            <p className="Home__Main-hero-subtitle">Accede a terapia online, grupos de apoyo, recursos educativos y más</p>
+            <h1 className="Home__Main-hero-title">
+              Tu bienestar mental al alcance de tu mano
+            </h1>
+            <p className="Home__Main-hero-subtitle">
+              Accede a terapia online, grupos de apoyo, recursos educativos y
+              más
+            </p>
             <ul className="Home__Main-hero-list">
-              <li className="Home__Main-hero-list-item">Comunicación en línea con profesionales de confianza</li>
-              <li className="Home__Main-hero-list-item">Grupos de apoyo disponibles 24/7</li>
-              <li className="Home__Main-hero-list-item">Recursos educativos personalizados</li>
+              <li className="Home__Main-hero-list-item">
+                <span className="material-symbols-outlined">asterisk</span>
+                Comunicación en línea con profesionales de confianza.
+              </li>
+              <li className="Home__Main-hero-list-item">
+                <span className="material-symbols-outlined">asterisk</span>
+                Grupos de apoyo disponibles 24/7.
+              </li>
+              <li className="Home__Main-hero-list-item">
+                <span className="material-symbols-outlined">asterisk</span>
+                Recursos educativos personalizados.
+              </li>
             </ul>
           </div>
           <div className="Home__Main-imageContainer">
-            <img className="Home__Main-imageContainer-image" src={img} alt="Persona meditando" />
+            <img
+              className="Home__Main-imageContainer-image"
+              src={img}
+              alt="Persona meditando"
+            />
           </div>
         </section>
         <div className="arrow-down">
