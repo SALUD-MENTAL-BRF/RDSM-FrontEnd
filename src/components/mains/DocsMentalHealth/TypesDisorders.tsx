249a1942830074d2345dd8ce7dc1adad215e5c89
Rodriasd <veronrodrigo98@gmail.com>
2024-06-12 14:56:36 -0300

    the description according to the disorder was added to the disorder types page

--- src/components/mains/DocsMentalHealth/TypesDisorders.tsx
+++ src/components/mains/DocsMentalHealth/TypesDisorders.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "../../../assets/style/DocsMentalHealth/InfoMentalHealth.css";
+
 export const TypesDisorders: React.FC = () => {
 
     return (
         <main className="container-typeDisorders">
             <div>
-                <h5 className="text-center">Mental Health:</h5>
+                <h3 className="text-center">Problemas relacionados con la aplicación:</h3>
                 <section className="articles">
                     <article className="card-typeDisorders">
                         <div className="article-wrapper">
@@ -13,12 +14,13 @@ export const TypesDisorders: React.FC = () => {
                             <img src="https://picsum.photos/id/1011/800/450" alt="" />
                         </figure>
                         <div className="article-body">
-                            <h2>This is some title</h2>
+                            <h2>Estado de ánimo</h2>
                             <p>
-                            Curabitur convallis ac quam vitae laoreet. Nulla mauris ante, euismod sed lacus sit amet, congue bibendum eros. Etiam mattis lobortis porta. Vestibulum ultrices iaculis enim imperdiet egestas.
+                            Los trastornos del estado de ánimo afectan la forma en que las personas sienten y experimentan sus emociones diariamente. Incluyen la depresión mayor,
+                             el trastorno bipolar y el trastorno distímico. Estos trastornos pueden llevar a cambios significativos en el comportamiento, la energía y la capacidad para realizar actividades cotidianas.
                             </p>
                             <a href="#" className="read-more">
-                            Read more <span className="sr-only">about this is some title</span>
+                            Leer más <span className="sr-only">about Estado de ánimo</span>
                             <svg xmlns="http://www.w3.org/2000/svg" className="icon" viewBox="0 0 20 20" fill="currentColor">
                                 <path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" />
                             </svg>
@@ -32,12 +34,12 @@ export const TypesDisorders: React.FC = () => {
                             <img src="https://picsum.photos/id/1005/800/450" alt="" />
                         </figure>
                         <div className="article-body">
-                            <h2>This is some title</h2>
+                            <h2>Ansiedad</h2>
                             <p>
-                            Curabitur convallis ac quam vitae laoreet. Nulla mauris ante, euismod sed lacus sit amet, congue bibendum eros. Etiam mattis lobortis porta. Vestibulum ultrices iaculis enim imperdiet egestas.
+                            Los trastornos de ansiedad son condiciones de salud mental que provocan una preocupación y un miedo excesivos. Entre ellos se incluyen el trastorno de ansiedad generalizada (TAG), el trastorno de pánico, el trastorno de ansiedad social y el trastorno obsesivo-compulsivo (TOC). Estos trastornos pueden interferir significativamente con las actividades diarias y el bienestar general.
                             </p>
                             <a href="#" className="read-more">
-                            Read more <span className="sr-only">about this is some title</span>
+                            Leer más <span className="sr-only">about Ansiedad</span>
                             <svg xmlns="http://www.w3.org/2000/svg" className="icon" viewBox="0 0 20 20" fill="currentColor">
                                 <path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" />
                             </svg>
@@ -46,18 +48,18 @@ export const TypesDisorders: React.FC = () => {
                         </div>
                     </article>
                     <article className="card-typeDisorders">
-
                         <div className="article-wrapper">
                         <figure className="container-image-typeDisorders">
                             <img src="https://picsum.photos/id/103/800/450" alt="" />
                         </figure>
                         <div className="article-body">
-                            <h2>This is some title</h2>
-                            <p>
-                            Curabitur convallis ac quam vitae laoreet. Nulla mauris ante, euismod sed lacus sit amet, congue bibendum eros. Etiam mattis lobortis porta. Vestibulum ultrices iaculis enim imperdiet egestas.
+                            <h2>Trauma y el estrés</h2>
+                            <p className="mb-5">
+                            Los trastornos relacionados con el trauma y el estrés se desarrollan a partir de eventos traumáticos o estresantes.
+                             Incluyen el trastorno de estrés agudo y el trastorno de estrés postraumático (TEPT). Estos trastornos pueden causar síntomas como flashbacks, pesadillas y una grave angustia emocional que afecta la vida diaria.
                             </p>
                             <a href="#" className="read-more">
-                            Read more <span className="sr-only">about this is some title</span>
+                            Leer más <span className="sr-only">about Trauma y el estrés</span>
                             <svg xmlns="http://www.w3.org/2000/svg" className="icon" viewBox="0 0 20 20" fill="currentColor">
                                 <path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" />
                             </svg>
