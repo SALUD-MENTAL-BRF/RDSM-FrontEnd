00aff7b53f913b9266fcd742a3e1f7bbea3db87b
Rodriasd <veronrodrigo98@gmail.com>
2024-06-12 15:21:19 -0300

    was added the cards of the disorders of feeding

--- src/components/mains/DocsMentalHealth/TypesDisorders.tsx
+++ src/components/mains/DocsMentalHealth/TypesDisorders.tsx
@@ -5,8 +5,8 @@ export const TypesDisorders: React.FC = () => {
 
     return (
         <main className="container-typeDisorders">
-            <div>
-                <h3 className="text-center">Problemas relacionados con la aplicación:</h3>
+            <div className="mt-5">
+                <h1 className="text-center">Problemas relacionados con la aplicación:</h1>
                 <section className="articles">
                     <article className="card-typeDisorders">
                         <div className="article-wrapper">
@@ -65,6 +65,25 @@ export const TypesDisorders: React.FC = () => {
                             </svg>
                             </a>
                         </div>
+                        </div>
+                    </article>
+                    <article className="card-typeDisorders mb-5">
+                        <div className="article-wrapper">
+                            <figure className="container-image-typeDisorders">
+                                <img src="https://picsum.photos/id/103/800/450" alt="" />
+                            </figure>
+                            <div className="article-body">
+                                <h2>Alimentación</h2>
+                                <p className="mb-5">
+                                    Los trastornos de la alimentación son condiciones graves relacionadas con comportamientos alimentarios persistentes que afectan negativamente la salud, las emociones y la capacidad de funcionar en áreas importantes de la vida. Incluyen la anorexia nerviosa, la bulimia nerviosa y el trastorno por atracón. Estos trastornos pueden llevar a consecuencias físicas severas y requieren un enfoque integral para el tratamiento y la recuperación.
+                                </p>
+                                <a href="#" className="read-more">
+                                    Leer más <span className="sr-only">about Alimentación</span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="icon" viewBox="0 0 20 20" fill="currentColor">
+                                        <path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                    </svg>
+                                </a>
+                            </div>
                         </div>
                     </article>
                 </section>
