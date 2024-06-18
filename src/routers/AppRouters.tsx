9f7407852213cc63129431b922a37452ae1099ab
Cespedes Brian <cespedes.aguilera.brian@gmail.com>
2024-06-18 16:23:40 -0300

    Route for Profile page created

--- src/routers/AppRouters.tsx
+++ src/routers/AppRouters.tsx
@@ -4,16 +4,18 @@ import { RegisterPage } from '../pages/RegisterPage';
 import { LoginPage } from '../pages/LoginPage';
 import { PanelPatient } from '../pages/PanelPatient';
 import {InfoMentalHealth} from '../pages/InfoMentalHealth'
+import { ProfilePage } from '../pages/ProfilePage';
 
 export const AppRouters = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path='/register' element={<RegisterPage />}/>
-        <Route path='/login' element={<LoginPage />}/>
-        <Route path='/patient' element={<PanelPatient/>}/>
-        <Route path='/information' element={<InfoMentalHealth/>}/>
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/patient' element={<PanelPatient/>} />
+        <Route path='/information' element={<InfoMentalHealth/>} />
+        <Route path='/profile' element={<ProfilePage />} />
       </Routes>
     </BrowserRouter>
   );
