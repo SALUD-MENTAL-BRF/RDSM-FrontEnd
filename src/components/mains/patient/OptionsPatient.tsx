80a9f39a863c851d0a29d9b4b73cc917114c01e0
Rodriasd <veronrodrigo98@gmail.com>
2024-06-19 12:19:30 -0300

    added images to the panel patient

--- src/components/mains/patient/OptionsPatient.tsx
+++ src/components/mains/patient/OptionsPatient.tsx
@@ -5,12 +5,28 @@ import { Aside } from "../../../assets/style/aside/Aside";
 export const Options: React.FC = () => {
     return (
         <main className="container-patient">
-            <div>
-                <div className="col-1">
+            <div className="row w-100">
+                <section className="aside-patient col-1 d-flex justify-content-center p-3">
                     <Aside/>
-                </div>
+                </section>
+                <section className="col mt-5 options-container">
+                    <div className="row justify-content-space-between">
+                        <div className="col-3">
+                            <div>
+                                <img className="img-actividades" src="home-patient/actividades.webp" alt="" />
+                            </div>
+                            <div>
+                                <img className="img-diario" src="home-patient/diario.webp" alt="" />
+                            </div>
+                        </div>
+                        <div className="col-6 text-start">
+                            <div>
+                                <img className="imgPsicologo" src="home-patient/psicologo.webp" alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </div>
-            
         </main>
     );
 };
--- src/pages/PanelPatient.tsx
+++ src/pages/PanelPatient.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Options } from "../components/mains/patient/OptionsPatient";
-
+import { Header } from "../components/headers/Header";
 export const PanelPatient : React.FC = () => {
     return(
         <>  
-            {/* <Header/> */}
+            <Header/>
             <Options/>
         </>
     );
