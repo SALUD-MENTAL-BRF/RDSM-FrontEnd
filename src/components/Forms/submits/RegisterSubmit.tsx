19fc9e62c2934b5d54e4bbe43a9bccab062e0337
brian Céspedes aguilera <cespedes.aguilera.brian@gmail.com>
2024-06-29 15:44:40 -0300

    implemented CustomFetch

--- src/components/Forms/submits/RegisterSubmit.tsx
+++ src/components/Forms/submits/RegisterSubmit.tsx
@@ -1,6 +1,8 @@
 import { useContext } from "react";
-import Swal from "sweetalert2";
 import { AuthContext } from "../../../context/AuthProvider";
+import { CustomFetch } from '../../../api/CustomFetch';
+import Swal from "sweetalert2";
+
 interface FormState {
     username: string;
     email: string;
@@ -12,33 +14,25 @@ export const RegisterSubmit: React.FC<{ stateForm: FormState }> = ({ stateForm }
     const { login }: any = useContext(AuthContext);
 
     const handleSubmit: React.MouseEventHandler<HTMLButtonElement> = async (e) => {
-    e.preventDefault();
-    if (stateForm.password !== stateForm.confirmPassword) {
-        return Swal.fire({
-            title: "Error",
-            text: "Las contraseñas no coinciden",
-            icon: "error",
-            width: "50%",
-            padding: "1rem",
-            background: "#FFF",
-            grow: "row",
-        });
-    } else {
-        try {
-            const response = await fetch("http://localhost:3000/auth/register", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
+        e.preventDefault();
+        if (stateForm.password !== stateForm.confirmPassword) {
+            return Swal.fire({
+                title: "Error",
+                text: "Las contraseñas no coinciden",
+                icon: "error",
+                width: "50%",
+                padding: "1rem",
+                background: "#FFF",
+                grow: "row",
+            });
+        } else {
+            try {
+                const data = await CustomFetch("http://localhost:3000/auth/register", "POST", {
                     username: stateForm.username,
                     email: stateForm.email,
                     password: stateForm.password,
-                }),
-            });
+                });
 
-            if (response.ok) {
-                const data = await response.json();
                 login({ token: data.token, user: data.user });
 
                 Swal.fire({
@@ -52,14 +46,14 @@ export const RegisterSubmit: React.FC<{ stateForm: FormState }> = ({ stateForm }
                 });
 
                 setTimeout(() => {
-                    window.location.href = "/"
+                    window.location.href = "/";
                 }, 2000);
 
-            } else {
-                const data = await response.json();
-                return Swal.fire({
+            } catch (error) {
+                console.error("Error al registrar el usuario:", error);
+                Swal.fire({
                     title: "Error",
-                    text: data.message,
+                    text: "Hubo un problema al registrar el usuario",
                     icon: "error",
                     width: "50%",
                     padding: "1rem",
@@ -67,27 +61,14 @@ export const RegisterSubmit: React.FC<{ stateForm: FormState }> = ({ stateForm }
                     grow: "row",
                 });
             }
-
-        } catch (error) {
-            console.error("Error al registrar el usuario:", error);
-            Swal.fire({
-                title: "Error",
-                text: "Hubo un problema al registrar el usuario",
-                icon: "error",
-                width: "50%",
-                padding: "1rem",
-                background: "#FFF",
-                grow: "row",
-            });
         }
-    }
-};
+    };
 
-return (
-    <div className="botonRegister">
-        <button type="submit" onClick={handleSubmit} className="botonRegister">
-            Registro
-        </button>
-    </div>
-);
+    return (
+        <div className="botonRegister">
+            <button type="submit" onClick={handleSubmit} className="botonRegister">
+                Registro
+            </button>
+        </div>
+    );
 };
