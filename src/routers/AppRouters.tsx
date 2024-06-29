3001404c28aa87172a99a0f32450b8253b42b368
brian Céspedes aguilera <cespedes.aguilera.brian@gmail.com>
2024-06-29 14:57:49 -0300

    added logic for validation authState in Routes

--- src/routers/AppRouters.tsx
+++ src/routers/AppRouters.tsx
@@ -1,3 +1,4 @@
+import { useContext } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { LandingPage } from '../pages/LandingPage'
 import { RegisterPage } from '../pages/RegisterPage';
@@ -5,18 +6,21 @@ import { LoginPage } from '../pages/LoginPage';
 import { ProfilePage } from '../pages/ProfilePage';
 import { HomePage } from '../pages/HomePage';
 import {InfoMentalHealthPage} from '../pages/InfoMentalHealthPage'
+import { AuthContext } from '../context/AuthProvider'
 
 export const AppRouters = () => {
+  const { authState }: any = useContext(AuthContext);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path='/register' element={<RegisterPage />}/>
-        <Route path='/login' element={<LoginPage />}/>
+        <Route path='/register' element={authState ? <HomePage /> : <RegisterPage />}/>
+        <Route path='/login' element={authState ? <HomePage /> : <LoginPage />}/>
         {/* <Route path='/patient' element={<PanelPatient/>} /> */}
-        <Route path='/profile' element={<ProfilePage />} />
-        <Route path='/home' element={<HomePage/>}/>
-        <Route path='/information' element={<InfoMentalHealthPage/>}/>
+        <Route path='/home' element={authState ? <HomePage /> : <LoginPage />}/>
+        <Route path='/profile' element={authState ? <ProfilePage /> : <LoginPage />} />
+        <Route path='/information' element={authState ? <InfoMentalHealthPage/> : <LoginPage />}/>
       </Routes>
     </BrowserRouter>
   );
