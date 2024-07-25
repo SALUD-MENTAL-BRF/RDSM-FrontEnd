4c9becc70841f7546fa8e1baf8e8e799a7c1c4ad
brian Céspedes aguilera <cespedes.aguilera.brian@gmail.com>
2024-07-25 19:54:45 -0300

    Authentication context has been reintegrated

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,44 @@
+import { createContext, useState, useEffect, ReactNode, FC } from 'react';
+import { User, AuthContextProps } from './types/types';
+
+const AuthContext = createContext<AuthContextProps | undefined>(undefined);
+
+const AuthProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [token, setToken] = useState<string | null>(null);
+  const [isLogged, setIsLogged] = useState<boolean>(false);
+
+  useEffect(() => {
+    const savedUser = localStorage.getItem('user');
+    const savedToken = localStorage.getItem('token');
+    if (savedUser && savedToken) {
+      setUser(JSON.parse(savedUser));
+      setToken(savedToken);
+      setIsLogged(true);
+    }
+  }, []);
+
+  const login = (user: User, token: string) => {
+    setUser(user);
+    setToken(token);
+    setIsLogged(true);
+    localStorage.setItem('user', JSON.stringify(user));
+    localStorage.setItem('token', token);
+  };
+
+  const logout = () => {
+    setUser(null);
+    setToken(null);
+    setIsLogged(false);
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, token, isLogged, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export { AuthProvider, AuthContext };
---
+++ src/context/types/types.ts
@@ -0,0 +1,17 @@
+export interface User {
+  id: number;
+  username: string;
+  email: string;
+  googleId: string;
+  imageUrl: string;
+  password: string;
+  roleId: number;
+}
+
+export interface AuthContextProps {
+  user: User | null;
+  token: string | null;
+  isLogged: boolean;
+  login: (user: User, token: string) => void;
+  logout: () => void;
+}
