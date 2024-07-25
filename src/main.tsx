1bb6ffdac5e406a4b87760c25c1d60790e37db7c
brian Céspedes aguilera <cespedes.aguilera.brian@gmail.com>
2024-07-25 19:56:00 -0300

    Change of route in the provider import

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { AppRouters } from './routers/AppRouters'
-import { AuthProvider } from './context/AuthProvider'
+import { AuthProvider } from './context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
