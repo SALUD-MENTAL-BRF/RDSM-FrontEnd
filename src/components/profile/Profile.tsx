92d4685ba5af1ddb3d3e7c18ddecb9481a7d56ba
brian Céspedes aguilera <cespedes.aguilera.brian@gmail.com>
2024-07-29 19:09:17 -0300

    Depending on the roleId, certain user information is displayed.

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -5,10 +5,9 @@ import imageExample from "/image-example/imageUser.jpg";
 import useAuth from "../../hooks/useAuth";
 
 export const Profile: React.FC = () => {
+  const { user } = useAuth();
 
-  const { user } = useAuth()
-
-  console.log(user)
+  console.log(user);
 
   return (
     <>
@@ -20,46 +19,60 @@ export const Profile: React.FC = () => {
           <div className="col-10 col-sm-10 col-md-10 col-lg-11 col-xl-11 col-xxl-11 containerMainProfile__InformationUser">
             <div className="profile-header">
               <div className="user-info">
-                <img src={user?.imageUrl ? user.imageUrl : imageExample} alt="User" className="userImage" />
+                <img
+                  src={user?.imageUrl ? user.imageUrl : imageExample}
+                  alt="User"
+                  className="userImage"
+                />
                 <div>
                   <h2>{user?.username}</h2>
-                  <p>Genero: {user?.gender ? user?.gender : 'Puedes editar esta información si lo necesitas.'}</p>
-                  <p>Edad: {user?.age ? user?.age : 'Puedes editar esta información si lo necesitas.'}</p>
-                  <p>terapia: Debes ser un paciente para visualizar esta información</p>
-                  <p>Ultima Sesión: Debes ser un paciente para visualizar esta información.</p>
-                  <p>Terapeuta: Debes ser un paciente para visualizar esta informacion</p>
+                  <p>Email: {user?.email}</p>
                   <p>User ID: {user?.id}</p>
+                  {user?.roleId === 3 ? (
+                    <>
+
+                      <p>Terapia: Debes ser un paciente para visualizar esta información</p>
+                      <p>Última Sesión: Debes ser un paciente para visualizar esta información.</p>
+                      <p>Terapeuta: Debes ser un paciente para visualizar esta información</p>
+                    </>
+                  ) : (
+                    <p>Role: {user?.roleId === 1 ? 'Nuevo Usuario' : 'Paciente'}</p>
+                  )}
                 </div>
               </div>
             </div>
             <div className="profile-content row mt-5">
-              <div className="col-12 col-md-6 col-lg-4 profile-section">
-                <div className="treatment-plan">
-                  <h3>Plan de tratamiento</h3>
-                  <ul>
-                    <li>Medicación ISRS: sesiones semanales</li>
-                    <li>Larazepam: As needed</li>
-                  </ul>
-                </div>
-              </div>
-              <div className="col-12 col-md-6 col-lg-4 profile-section">
-                <div className="mental-health-stats">
-                  <h3>Estadísticas de salud mental</h3>
-                  <p>Nivel de ansiedad: Moderado</p>
-                  <p>Estado de ánimo: estable</p>
-                  <p>Estres: High</p>
-                </div>
-              </div>
-              <div className="col-12 col-md-6 col-lg-4 profile-section">
-                <div className="additional-stats">
-                  <h3>Estadísticas adicionales</h3>
-                  <ul>
-                    <li>Horas de Sueño: promedio 8 horas 15 minutos</li>
-                    <li>Actividad: promedio 8500 pasos</li>
-                    <li>Peso: 70 kg</li>
-                  </ul>
-                </div>
-              </div>
+              {user?.roleId === 3 ?? (
+                <>
+                  <div className="col-12 col-md-6 col-lg-4 profile-section">
+                    <div className="treatment-plan">
+                      <h3>Plan de tratamiento</h3>
+                      <ul>
+                        <li>Medicación ISRS: sesiones semanales</li>
+                        <li>Larazepam: As needed</li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="col-12 col-md-6 col-lg-4 profile-section">
+                    <div className="mental-health-stats">
+                      <h3>Estadísticas de salud mental</h3>
+                      <p>Nivel de ansiedad: Moderado</p>
+                      <p>Estado de ánimo: estable</p>
+                      <p>Estrés: Alto</p>
+                    </div>
+                  </div>
+                  <div className="col-12 col-md-6 col-lg-4 profile-section">
+                    <div className="additional-stats">
+                      <h3>Estadísticas adicionales</h3>
+                      <ul>
+                        <li>Horas de Sueño: promedio 8 horas 15 minutos</li>
+                        <li>Actividad: promedio 8500 pasos</li>
+                        <li>Peso: 70 kg</li>
+                      </ul>
+                    </div>
+                  </div>
+                </>
+              )}
             </div>
           </div>
         </div>
