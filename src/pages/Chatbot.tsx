4961428eb85f2d07533270602118716536519af5
Rodriasdev <veronrodrigo98@gmail.com>
2024-07-31 03:16:42 -0300

    added the component'BoxChat'

--- src/pages/Chatbot.tsx
+++ src/pages/Chatbot.tsx
@@ -3,6 +3,7 @@ import { BoxChat } from "../components/main/chatbot/BoxChat";
 import { Header } from "../components/headers/Header";
 import "../assets/style/chatbot.css";
 export const Chatbot: React.FC = () => {
+    
     return(
         <>
             <Header/>
