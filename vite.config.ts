987b73583836171f78442dea9c725905d1bbe679
Cespedes Brian <cespedes.aguilera.brian@gmail.com>
2024-08-21 11:48:46 -0300

    Using the VITE_PORT environment variable

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import { environments } from './src/config/environments';
 
-// Carga las variables de entorno desde el archivo .env
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
 
-// Asegúrate de que la variable de entorno esté definida
-
-
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: environments.PORT,
-  },
+  return {
+    plugins: [react()],
+    server: {
+      port: parseInt(env.VITE_PORT),
+    },
+  };
 });
