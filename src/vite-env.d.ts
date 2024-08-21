9b82aefbd1042fd83970c53cb111ffa35e85c6c7
Cespedes Brian <cespedes.aguilera.brian@gmail.com>
2024-08-21 11:48:15 -0300

    Setting environment variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_PORT: string;
+  readonly VITE_API_URL: string;
+  // otras variables de entorno que tengas
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
