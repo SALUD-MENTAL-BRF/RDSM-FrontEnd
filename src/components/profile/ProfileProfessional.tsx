c7b5b4384dcb866425e2bc25b53ea1c70f51e794
Rodriasd <veronrodrigo98@gmail.com>
2024-08-26 15:01:53 -0300

    fixed code

--- src/components/profile/ProfileProfessional.tsx
+++ src/components/profile/ProfileProfessional.tsx
@@ -85,10 +85,10 @@ export const ProfileProfessional = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-heart-fill" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
                             </svg>
+                            <div className="text-end">
+                                <button className="btn btn-outline-dark">Consultar</button>
+                            </div>
                         </div>
-                    </div>
-                    <div className="col-12">
-                        
                     </div>
                 </div>
             </section>
