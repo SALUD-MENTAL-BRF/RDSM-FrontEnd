432b908e93716eaec1662c809a9e592f683f98d9
Rodriasd <veronrodrigo98@gmail.com>
2024-09-06 16:15:52 -0300

    fixed code

--- src/components/profile/professional/ProfileProfessional.tsx
+++ src/components/profile/professional/ProfileProfessional.tsx
@@ -46,7 +46,7 @@ export const ProfileProfessional = () => {
                         <div>Cargando...</div> 
                     )}
                     <section>
-                        <div role="button" className="rounded-1 bg-dark text-white d-flex justify-content-center">
+                        <div role="button" style={{backgroundColor: "#10b981"}} className="rounded-1 text-white d-flex justify-content-center h-100">
                             <p className="mt-1">Ver publicaciones</p>
                             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="mt-2 ms-1 bi bi-arrow-down" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1"/>
