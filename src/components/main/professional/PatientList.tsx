b21dec69fa0e7904bacdcc7acf1f45f4186d37a5
Rodriasd <veronrodrigo98@gmail.com>
2024-09-11 15:09:47 -0300

    added fullscreen or fulscreen exit in the page of the patient list

--- src/components/main/professional/PatientList.tsx
+++ src/components/main/professional/PatientList.tsx
@@ -1,12 +1,20 @@
 import { useNavigate } from "react-router-dom";
 import '../../../assets/style/professional/PatientList.css'
+import { useState } from "react";
+
 export const PatientList = () => {
 
     const navigate = useNavigate()
+    const [fullscreenState, setfullscreen] = useState<boolean>(true)
 
+    const changeFulscree = () => {
+        return setfullscreen(!fullscreenState)
+    } 
     return(
         <main className="container-fluid">
             <div className="row w-100">
+
+
                 <section className="col-2 col-sm-2 col-md-2 col-lg-1 col-xl-1 col-xxl-1 text-center">
                         <div role='button' onClick={() => navigate('/home')}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-arrow-bar-left mt-4" viewBox="0 0 16 16">
@@ -15,16 +23,39 @@ export const PatientList = () => {
                             <h6 className='ms-1'>Atrás</h6>
                         </div>
                 </section>
+                {
+                fullscreenState == true ?
                 <section className="patient-assigned col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 col-xxl-4 mt-4 rounded-4 ms-2 m-2">
                     <div className="row">
-                         <h4 className="text-center fw-bold mt-2">Pacientes asignados</h4>
-                        <div className="container-imgPatient d-flex">
+                        <div className="d-flex align-items-center w-100">
+                            <h4 className="w-100 text-center fw-bold mt-2">Lista de pacientes</h4>
+                            <div onClick={changeFulscree} title="Minimizar">
+                                <svg   role="button" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-fullscreen-exit" viewBox="0 0 16 16">
+                                    <path d="M5.5 0a.5.5 0 0 1 .5.5v4A1.5 1.5 0 0 1 4.5 6h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5m5 0a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 10 4.5v-4a.5.5 0 0 1 .5-.5M0 10.5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 6 11.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5m10 1a1.5 1.5 0 0 1 1.5-1.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0z"/>
+                                </svg>
+                            </div>
+                        </div>
+                        <div role="button" className="container-imgPatient d-flex">
                             <div>
                                 <img className="imgPatient rounded-5" src="/image-example/imageUser.jpg" alt="" />
                             </div>
                         </div>
                     </div>
                 </section>
+                : 
+                <section className="patient-assigned-exit col-2 rounded-4 ms-2 m-2">
+                    <div className="row">
+                        <div className="d-flex align-items-center w-100">
+                            <h4 className="w-100 text-center fw-bold mt-4">Lista de pacientes</h4>
+                            <div className="mt-3 ms-2" onClick={changeFulscree} title="Minimizar">
+                                <svg role="button" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrows-fullscreen" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707m4.344 0a.5.5 0 0 1 .707 0l4.096 4.096V11.5a.5.5 0 1 1 1 0v3.975a.5.5 0 0 1-.5.5H11.5a.5.5 0 0 1 0-1h2.768l-4.096-4.096a.5.5 0 0 1 0-.707m0-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707m-4.344 0a.5.5 0 0 1-.707 0L1.025 1.732V4.5a.5.5 0 0 1-1 0V.525a.5.5 0 0 1 .5-.5H4.5a.5.5 0 0 1 0 1H1.732l4.096 4.096a.5.5 0 0 1 0 .707"/>
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                }                
             </div>
         </main>
     )
