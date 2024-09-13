d435e20c70f07784be2be7bcef7995fd0bf05a78
Cespedes Brian <cespedes.aguilera.brian@gmail.com>
2024-09-13 14:39:28 -0300

    Elimination of unnecessary lines

--- src/pages/SuperAdmin.tsx
+++ src/pages/SuperAdmin.tsx
@@ -1,9 +1,7 @@
 import React from "react"
 import { Sidebar } from "../components/admin/Sidebar/Sidebar";
 import { Dashboard } from "../components/admin/Content/Dashboard";
-// import '../assets/style/admin/Content/Dashboard.css';
-import '../assets/style/admin/index.css';
-import '../assets/style/admin/Sidebar.css'
+
 export const SuperAdmin: React.FC = (): React.ReactNode => {
 
     return (
