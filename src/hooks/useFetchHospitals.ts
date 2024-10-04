10346880691705dd50ca8496217bd95a5e5783f6
Cespedes Brian <cespedes.aguilera.brian@gmail.com>
2024-10-04 15:17:54 -0300

    A hook was added to bring the hospitals

---
+++ src/hooks/useFetchHospitals.ts
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { Hospital } from "../types/Hospital";
+import { CustomFetch } from "../api/CustomFetch";
+
+
+
+export const useFetchHospitals = () => {
+  const [hospitals, setHospitals] = useState<Hospital[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchHospitals = async () => {
+      try {
+
+        const response = await CustomFetch(`${import.meta.env.VITE_API_URL}hospitals`, 'GET');
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        setHospitals(data);
+
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err.message);
+        } else {
+          setError("An unknown error occurred");
+        }
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchHospitals();
+  }, []);
+
+  return { hospitals, loading, error };
+}
