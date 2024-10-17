1c5d39093165cd75b4a5743e64dc9519223811d2
Cespedes Brian <cespedes.aguilera.brian@gmail.com>
2024-10-17 16:46:47 -0300

    expiration token

--- src/reducers/authReducers.tsx
+++ src/reducers/authReducers.tsx
@@ -1,20 +1,23 @@
 import { types } from "../types/typesUser";
-import { AuthState } from "../context/types/types.tsx";
+import { AuthState } from "../context/types/types";
 
 export const authReducer = (state: AuthState, action: any): AuthState => {
   switch (action.type) {
     case types.LOGIN:
-      const { token } = action.payload;
-      localStorage.setItem("token", token);
+      localStorage.setItem("token", action.payload.token);
+      localStorage.setItem("tokenExpiration", (Date.now() + 7 * 24 * 60 * 60 * 1000).toString());
       localStorage.setItem("isLogged", "true");
+      localStorage.setItem("lastLogin", Date.now().toString());
       return {
         ...state,
         isLogged: true,
-        token,
+        token: action.payload.token,
       };
 
     case types.LOGOUT:
       localStorage.removeItem("token");
+      localStorage.removeItem("tokenExpiration");
+      localStorage.removeItem("lastLogin");
       localStorage.setItem("isLogged", "false");
       return {
         isLogged: false,
