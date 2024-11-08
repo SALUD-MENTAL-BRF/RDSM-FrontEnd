bd19a7b2696fb3ff6ae0462118ce468ef963efa1
Cespedes Brian <cespedes.aguilera.brian@gmail.com>
2024-11-08 11:56:48 -0300

    console.log was deleted

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -99,9 +99,7 @@ export const Profile: React.FC = () => {
     } finally {
       setIsLoading(false);
     }
-  };  
-
-  console.log(user)
+  };
 
   return (
     <div className='container-fluid profile-page'>
