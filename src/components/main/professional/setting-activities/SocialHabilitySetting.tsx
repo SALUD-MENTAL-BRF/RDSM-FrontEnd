3e9d084820c83c7ddfcd50ed513abee3f3822e22
Rodriasd <veronrodrigo98@gmail.com>
2024-11-08 09:55:36 -0300

    added description in the page of the setting of the activity of the social hability

--- src/components/main/professional/setting-activities/SocialHabilitySetting.tsx
+++ src/components/main/professional/setting-activities/SocialHabilitySetting.tsx
@@ -1,24 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import'../../../../assets/style/activities/SocialHabilitySetting.css';
 
 export const SocialHabilitySetting = () => {
     const [selectState, setSelectState] = useState({
         age: "",
         genre: "",
-        Complejidad: ""
+        complexity: "",
+        personality: ""
     });
     const [ages, _setAges] = useState<Array<number>>(Array.from({ length: 80 }, (_, i) => i + 1));
 
     const handleSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
         const {name, value} = event.target
-        setSelectState(prevstate => ({...prevstate, [name]: value}));         
+        setSelectState(prevstate => ({...prevstate, [name]: value})); 
+        console.log(selectState);
+                
     };
 
     return (
         <div className="text-center">
             <div className="row d-flex justify-content-center">
                 <label className='text-center' htmlFor="age">Edad</label>
-                <select onChange={handleSelect} className="form-select w-25 mt-1 text-center select-settingSocialHability" name="age" id="age">
+                <select onChange={handleSelect} value={selectState.age} className="form-select w-25 mt-1 text-center select-settingSocialHability" name="age" id="age">
                     <option value="">Selecciona una edad...</option>
                     {
                         ages.map((age) => (
@@ -27,7 +30,8 @@ export const SocialHabilitySetting = () => {
                     }
                 </select>
                 <label htmlFor="genre">Género</label>
-                <select onChange={handleSelect} className="form-select w-25 text-center" name="genre" id="genre">
+                <select onChange={handleSelect} value={selectState.age} className="form-select w-25 text-center select-settingSocialHability" name="genre" id="genre">
+                <option value="">Selecciona un género...</option>
                     <option value="Masculino">
                         Masculino
                     </option>
@@ -36,13 +40,26 @@ export const SocialHabilitySetting = () => {
                     </option>
                 </select>
                 <label htmlFor="complexity">Complejidad</label>
-                <select name="complexity" className="form-select w-25 text-center" id="complexity">
+                <select onChange={handleSelect} value={selectState.complexity} name="complexity" className="form-select w-25 text-center select-settingSocialHability" id="complexity">
+                <option value="">Selecciona la complejidad...</option>
                     <option value="Fácil">Fácil</option>
                     <option value="Normal">Normal</option>
                     <option value="Difícil">Difícil</option>
                 </select>
+                <label htmlFor="personality">Personalidad</label>
+                <select onChange={handleSelect} value={selectState.personality} name="personality" className='form-select w-25 text-center select-settingSocialHability' id="">
+                    <option value="">Selecciona una personalidad...</option>
+                    <option value="Introvertido">Introvertido</option>
+                    <option value="Extrovertido">Extrovertido</option>
+                </select>
             </div>
             <button className="btn btn-primary mt-4">Guardar</button>
+            <section className='ms-5'>
+                <h4 className='text-start mt-5 title-settingSocialHability'>Descripción:</h4>
+                <p className='text-start ms-5 text-settingSocialHability'>Estas configuraciones afectaran la respuesta que le dara la Inteligencia Artificial al paciente, por lo tanto,
+                    asegurece de ajustarlo según el paciente lo necesite.
+                </p>
+            </section>
         </div>
     );
 };
