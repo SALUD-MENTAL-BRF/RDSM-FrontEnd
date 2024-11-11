518a7eea87f33e599d89ef3adbb5a06476a576e4
Cespedes Brian <cespedes.aguilera.brian@gmail.com>
2024-11-11 15:50:42 -0300

    console.log was deleted

--- src/components/panelHospital/MedicationManagement/modalMedicationManagement.tsx
+++ src/components/panelHospital/MedicationManagement/modalMedicationManagement.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export const ModalMedicationManagement:FC<Props> = ({ toggleModal }) => {
 
-  console.log(toggleModal)
+  
 
   return (
     <div className={styles.modalOverlay}>
